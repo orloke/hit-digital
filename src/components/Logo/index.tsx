8e340d6ec27b53f20c859708e873a81257ec67dc
Junior Dering <juniordering@hotmail.com>
2022-09-22 20:03:02 -0400

    feat: adding link to logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from '../../../public/assets/logo.png';
 import { Container } from './styles';
 
 export function Logo() {
   return (
     <Container>
-      <Image src={logo} />
+      <Link href="/">
+        <Image src={logo} />
+      </Link>
     </Container>
   );
 }
